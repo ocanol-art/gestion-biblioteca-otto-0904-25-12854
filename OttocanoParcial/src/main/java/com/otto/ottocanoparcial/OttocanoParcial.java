44f88e9c4c821f36a36b8d722210871c18a7cb3b
umg <umg@HUE-LAB-1C2.labhue.local>
2026-04-25 11:12:52 -0600

    creacion

---
+++ OttocanoParcial/src/main/java/com/otto/ottocanoparcial/OttocanoParcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.otto.ottocanoparcial;
+
+/**
+ *
+ * @author umg
+ */
+public class OttocanoParcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
